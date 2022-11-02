046aa5a9c7bef4219be762b7c67ae528b24a709f
Dmitry Vinichenko <zuko59@yandex.ru>
2022-11-02 23:03:58 +0500

    Review 1 fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,27 @@
 const editElem = document.querySelector(".profile__edit-button");
 const popupElem = document.querySelector(".popup");
-const popupCloseElem = popupElem.querySelector(".popup__button_action_close");
+const popupCloseElem = popupElem.querySelector(".popup__button-close");
 const reactElem = document.querySelector(".gallery__card-button");
-const saveElem = popupElem.querySelector(".popup__button_action_save");
 const form = document.querySelector(".popup__form");
 let owner = document.querySelector(".profile__owner");
 let statys = document.querySelector(".profile__status");
 let fieldOwner = document.querySelector(".popup__form-field_type_owner");
 let fieldStatus = document.querySelector(".popup__form-field_type_status");
 
-editElem.addEventListener("click", () => {
-  popupElem.classList.add("popup_opened");
+function fillFields() {
+  popupElem.classList.toggle("popup_opened");
   fieldOwner.value = owner.textContent;
   fieldStatus.value = statys.textContent;
-});
+}
 
-popupCloseElem.addEventListener("click", () => {
-  popupElem.classList.remove("popup_opened");
-});
-
-reactElem.addEventListener("click", () => {
-  reactElem.classList.toggle("gallery__card-button_type_marked");
-});
-
-saveElem.addEventListener("click", () => {
+function changeInfo() {
   owner.textContent = fieldOwner.value;
   statys.textContent = fieldStatus.value;
   popupElem.classList.remove("popup_opened");
-});
+}
+
+editElem.addEventListener("click", fillFields);
+
+popupCloseElem.addEventListener("click", fillFields);
+
+form.addEventListener("submit", changeInfo);
