179357806de0d95454444b0a1e0800751c03f43a
Dmitry Vinichenko <zuko59@yandex.ru>
2023-01-27 23:12:07 +0500

    ревью 1 правки

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,12 +3,12 @@ import Popup from "../components/Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._imageTitle = this._element.querySelector(".popup__image-caption");
+    this._imageLink = this._element.querySelector(".popup__image");
   }
 
   open(name, link) {
     super.open();
-    this._imageTitle = this._element.querySelector(".popup__image-caption");
-    this._imageLink = this._element.querySelector(".popup__image");
     this._imageTitle.textContent = name;
     this._imageLink.src = link;
     this._imageLink.alt = name;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
 import PopupWithImage from "../components/PopupWithImage.js";
-import { initialCards, validationData } from "../components/data.js";
+import { initialCards, validationData } from "../utils/data.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
 
